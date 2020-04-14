74e789852faf2e7e171677b41c76b8347f112cd8
gertsfert <gertsfert@gmail.com>
2020-04-14 20:32:05 +1000

    Move m2m tag/note field to note (from tag)

--- mysite/graph_notes/models.py
+++ mysite/graph_notes/models.py
@@ -4,22 +4,23 @@ from django.db import models
 from django.utils import timezone
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_time = models.DateTimeField("time published")
     modified_time = models.DateTimeField("last modified")
 
+    tags = models.ManyToManyField(Tag)
+
     def __str__(self):
         return self.title
 
     def was_published_recently(self):
         return self.published_time >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    notes = models.ManyToManyField(Note)
-
-    def __str__(self):
-        return self.name
